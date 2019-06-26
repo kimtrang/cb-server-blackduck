a4f5902c7740887b2efde70a1895982c8eb0cc26
Kim Trang <kim.trang@couchbase.com>
2019-06-26 14:56:42 -0700

    Revert back changes, not working

--- cb-3rdparty-bl-scan.sh
+++ cb-3rdparty-bl-scan.sh
@@ -3,25 +3,25 @@
 
 #!/bin/bash
 
-export WORKSPACE=$(pwd)
 rm -rf ${WORKSPACE}/thirdparty-src
-mkdir -p ${WORKSPACE}/thirdparty-src/deps/go/src
+mkdir -p ${WORKSPACE}/thirdparty-src/deps
 cd ${WORKSPACE}/thirdparty-src/deps
+
+export GOROOT=/usr/local/go
+export GOPATH=${WORKSPACE}/thirdparty-src/deps/
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 echo "dep init ..."
 /usr/local/go/bin/dep init
 
 pwd; ls -la
 
-cd ${WORKSPACE}/thirdparty-src/deps/go/src
+cd ${WORKSPACE}/thirdparty-src/deps
 
 #git clone git://github.com/couchbasedeps/breakpad.git
 #git clone git://github.com/couchbasedeps/grpc.git
 git clone git://github.com/couchbasedeps/flatbuffers.git
 #git clone git://github.com/couchbasedeps/protoc-gen-go.git
-export GOROOT=/usr/local/go
-export GOPATH=${WORKSPACE}/thirdparty-src/deps/
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
 
 #export PATH=$PATH:/usr/local/go/bin
 bash <(curl -s https://detect.synopsys.com/detect.sh) \
@@ -30,10 +30,10 @@ bash <(curl -s https://detect.synopsys.com/detect.sh) \
 --detect.project.name=CBServer \
 --detect.project.version.name=6.5.0 \
 --detect.project.codelocation.prefix=CBServer \
---detect.source.path=${WORKSPACE}/thirdparty-src/deps/go/src \
+--detect.source.path=${WORKSPACE}/thirdparty-src/deps \
 --detect.go.dep.path=/usr/local/go/bin/go \
 --detect.go.run.dep.init=true \
---detect.cleanup=true \
+--detect.cleanup=false \
 --detect.report.timeout=1800000000 \
 --detect.pipenv.path=/usr/local/bin/pipenv \
 --detect.cpan.path=/usr/bin/cpan \
